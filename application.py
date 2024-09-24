968e07422dedbc6d3d57710ac3764f282fcd253f
gurdeep330 <gurdeep330@gmail.com>
2024-09-24 12:59:11 +0200

    updte with Flask

--- application.py
+++ application.py
@@ -1,3 +1,14 @@
-import streamlit as st
+# import streamlit as st
 
-st.write('Hello, world!!')
+# st.write('Hello, world!!')
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run()
